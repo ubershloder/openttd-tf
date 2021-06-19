0693e49e530a45dca841bb5a0901f45a170b6eec
Ivan Karpenko <ubersholder@gmail.com>
2021-06-20 00:26:52 +0300

    New openttd version update

--- openttd.sh
+++ openttd.sh
@@ -11,8 +11,8 @@ fi
 
 sudo apt-get build-dep openttd -y
 sudo apt-get install libsdl2-2.0-0 -y
-wget https://cdn.openttd.org/openttd-releases/1.10.3/openttd-1.10.3-linux-ubuntu-focal-amd64.deb
-sudo dpkg -i openttd-1.10.3-linux-ubuntu-focal-amd64.deb
+wget https://cdn.openttd.org/openttd-releases/1.11.2/openttd-1.11.2-linux-ubuntu-focal-amd64.deb
+sudo dpkg -i openttd-1.11.2-linux-ubuntu-focal-amd64.deb
 sudo apt-get -f install -y
 sudo apt-get install -y openttd-opengfx
 
